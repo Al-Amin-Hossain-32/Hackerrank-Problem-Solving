94cd2a50c3a520faa42159f0dd0d1d304a84a611
Al Amin Hossain <105510011+Al-Amin-Hossain-32@users.noreply.github.com>
2022-10-15 23:56:43 +0600

    Update medium.js

--- Problem Solving/medium.js
+++ Problem Solving/medium.js
@@ -60,3 +60,31 @@ let fact = BigInt(1)
     }
     console.log(fact.toString())
 }
+
+
+***###// Complete the nonDivisibleSubset function below.
+
+function nonDivisibleSubset(k, s) {
+  // Write your code here
+  let values = new Array(k).fill(0);
+  let result = 0;
+
+  s.reduce((target, item, index) => {
+    values[item % k] += 1;
+
+    return target;
+  }, []);
+
+  for (let i of Array.from(
+    { length: (k + 1) / 2 - 1 },
+    (value, index) => index + 1
+  )) {
+    result += Math.max(values[i], values[k - i]);
+  }
+
+  !(k % 2) && !!values[k / 2] && (result += 1);
+
+  values[0] && (result += 1);
+
+  return result;
+}
