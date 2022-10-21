c74bc8c5edd70a8f4c50eacc650ec3581a0ea7d2
Al Amin Hossain <105510011+Al-Amin-Hossain-32@users.noreply.github.com>
2022-10-21 20:41:39 +0600

    Update medium.js

--- Problem Solving/medium.js
+++ Problem Solving/medium.js
@@ -88,3 +88,46 @@ function nonDivisibleSubset(k, s) {
 
   return result;
 }
+
+// Problem : Bigger is Greater 
+// Lexicographic order
+
+
+function biggerIsGreater(s) {
+    // Write your code here
+let length = s.length;
+  let otherStr = "";
+  let ins = 0;
+  let greaterC = "";
+  for (let i = s.length - 1; i >= 0; i--) {
+    let cur = s[i];
+    if (cur < s[i + 1]) {
+      ins = i;
+      for (let j = ins; j < s.length; j++) {
+        if (greaterC <= cur) {
+          greaterC = s[j];
+        }
+        if (s[j] < greaterC && s[j] > cur) {
+          greaterC = s[j];
+        }
+      }
+      break;
+    } else {
+      otherStr += s[i];
+    }
+  }
+  otherStr += s[ins];
+  otherStr = otherStr.replace(greaterC, "");
+  otherStr = otherStr.split("").sort().join("");
+  if (!greaterC) {
+    return "no answer";
+  }
+  let newStr = "";
+  for (let i = 0; i < length; i++) {
+    if (i < ins) newStr += s[i];
+    if (i === ins) newStr += greaterC;
+  }
+  newStr += otherStr;
+  return newStr;
+
+}
