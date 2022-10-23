1dba187d65487ed2f0d09e55cf6312ff254533dd
Al Amin Hossain <105510011+Al-Amin-Hossain-32@users.noreply.github.com>
2022-10-23 14:05:37 +0600

    Update solve.js

--- Problem Solving/solve.js
+++ Problem Solving/solve.js
@@ -29,3 +29,22 @@ function birthdayCakeCandles(arr){
     return velas.length;
 }
 
+// getTotalX function 
+
+function getTotalX(a, b) {
+    // Write your code here
+   let result = 0;
+    let index = 1;
+    let cloneA = a.splice(1, a.length); // clone new array of a but not a[0]
+    while(a[0] * index <= b[0]) {
+        if(
+         cloneA.every(item => (a[0] * index) % item === 0)
+          &&
+          b.every(item => item % (a[0] * index) === 0)
+         ) {
+            result++;
+        }
+        index++;
+    }
+    return result;
+}
