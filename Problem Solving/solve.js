fd552f0d4a3e55a54e3ec42942506f44ba29da0d
Al Amin Hossain <105510011+Al-Amin-Hossain-32@users.noreply.github.com>
2022-10-24 15:54:26 +0600

    Update solve.js

--- Problem Solving/solve.js
+++ Problem Solving/solve.js
@@ -48,3 +48,66 @@ function getTotalX(a, b) {
     }
     return result;
 }
+
+// Staircase javascript hackerrank solution
+function staircase(n) {
+    // Write your code here
+    let string = "";
+    for(let i=1; i<=n; i++){
+        for(let j=1; j<= n-i; j++){
+            string += " ";
+        }
+        for(let k=1; k<=i; k++){
+            string += "#";
+        }
+        string += "\n";
+    }
+    return console.log(string);
+}
+
+
+// breaking the records  
+
+function breakingRecords(scores) {
+    // Write your code here
+    let highScore = scores[0];
+    let lowScore = scores[0];
+    let highCount = 0;
+    let lowCount = 0;
+    for(let i=1; i<scores.length; i++){
+        if(scores[i] > highScore){
+            highScore = scores[i];
+            highCount++;
+        }else if(scores[i] < lowScore){
+            lowScore = scores[i];
+            lowCount++;
+        }
+    }
+    return [highCount, lowCount];
+}
+
+// Subarray Division 
+
+function birthday(s, d, m) {
+    let num = s;
+    let nums = [];
+    let count = 0;
+    const add = (arr) => arr.reduce((a, b) => a + b, 0);
+    for (let i = 0; i < s.length; i++) {
+      let arrayElement = num.slice(0 + i, m + i);
+      nums.push(arrayElement);
+    }
+    if(num.length===1 && num[0]===d){
+        count++;
+    }else{
+      nums.forEach((el) => {
+          if (add(el) === d) {
+            count++;
+          }
+        });
+    }
+    
+  
+    return count;
+}
+console.log(birthday(s, d, m));
