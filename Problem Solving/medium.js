038bd373d98a2e835506b625f4a9336d7c614e10
Al Amin Hossain <105510011+Al-Amin-Hossain-32@users.noreply.github.com>
2022-10-24 16:32:15 +0600

    Update medium.js

--- Problem Solving/medium.js
+++ Problem Solving/medium.js
@@ -131,3 +131,48 @@ let length = s.length;
   return newStr;
 
 }
+
+//Absolute Permutation hackerrank solution javascript
+
+function absolutePermutation(n, k) {
+    // Write your code here
+     const D = {}
+    for (let i=1; i <= n; i++)
+        if (i-k > 0 && !D[i-k]) D[i-k] = i
+        else if (i+k <= n && !D[i+k]) D[i+k] = i
+        else return [-1]
+    return Object.values(D)
+
+}
+
+
+// Highest Value Palindrome
+
+function highestValuePalindrome(s, n, k) {
+    // Write your code here
+if (n === 1) return k == 1 ? '9' : '-1'
+            
+    let arr = [...s]
+    let c = Array(n).fill(0)
+    let x = Math.floor(n/2) + (n&1)
+    
+    for (let i = 0; i < x; i++) {
+        if (arr[i] !== arr[n-1-i]) {
+            arr[i] = arr[n-1-i] = Math.max(arr[i], arr[n-1-i])
+            c[i]++
+            k--
+        }
+        if (k < 0) return '-1'
+    }
+    
+    for (let i = 0; i < x; i++) {
+        if (arr[i] != '9') {
+            let cost = c[i] === 1 || i === (n-1-i) ? 1 : 2
+            if (k >= cost) {
+                arr[i] = arr[n-1-i] = '9'
+                k -= cost
+            }
+        }
+    }
+    return arr.join('')
+}
